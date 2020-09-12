1d694d64c8b5f75d98f6c1ca6ea3242bd04020c4
bet9nz9 <tomimo1tokoso@gmail.com>
2020-09-12 13:38:24 +0300

    fixed task 2

--- Lab1/task2.js
+++ Lab1/task2.js
@@ -1,20 +1,12 @@
 let string = prompt("Введите строку ", 1)
 
-fragmentArray = new Array()
-fragment = ""
-for(let i=0; i<=string.length; i++){
-    if(fragment.length==3){
-        fragmentArray.push(fragment)
-        fragment = string[i]
-    }else{
-        fragment+=string[i]
-    }
-}
+fragmentArray = string.split(" ")
+
 console.log("Fragments before: "+fragmentArray)
 for(let i=0; i<fragmentArray.length;i++){
     fragmentArray[i] = fragmentBuilder(fragmentArray[i])
 }
-console.log("Fragments after: "+fragmentArray)
+console.log("Fragments after: "+fragmentArray.sort())
 
 function fragmentBuilder(str){
     trigger = true
@@ -23,14 +15,12 @@ function fragmentBuilder(str){
     while(trigger){
         if(str[0]!=sym && str[2]!=sym ){
             resultString+=str[0]+sym+str[2]
-            // str[1] = sym
-            // console.log("Sym : "+sym)
             trigger = false
         }else{
             sym = getRandomSymbol()
         }
     }
-    // console.log("Str in fun: "+resultString)
+
     return resultString
 }
 
